3998f51993ecb28b0ae7482ea34bd3f09f5a7765
Zhangyc <1332234917@qq.com>
2020-01-20 19:21:28 +0800

    Initial commit

---
+++ app/src/main/java/com/zyc/mywanandroid/MainActivity.java
@@ -0,0 +1,13 @@
+package com.zyc.mywanandroid;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
+}
